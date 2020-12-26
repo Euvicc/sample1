4a9620ce9ac46bf2a2c78b126b8409ffef0b8913
Euvicc <euvicc.works@gmail.com>
2020-12-27 00:03:04 +0900

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+basic.show_string("START!")
+
+def on_forever():
+    pass
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
+basic.showString("START!")
+basic.forever(function () {
+	
+})
